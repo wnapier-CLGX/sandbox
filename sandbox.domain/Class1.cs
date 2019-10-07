f8c7d0e7c885c1ff9506c4a4d4b0ef0170a6790f
William Napier <wnapier@corelogic.com>
2019-10-07 11:31:01 -0500

    FOO!

--- sandbox.domain/Class1.cs
+++ sandbox.domain/Class1.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Linq;
 
 namespace sandbox.domain
 {
     public class Class1
     {
+        public string Foo(string Bar)
+        {
+            return Bar.ToArray().Reverse().ToString();
+        }
     }
 }
